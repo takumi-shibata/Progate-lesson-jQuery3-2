20bfb663d21cac8b34267307409168275576f9bb
takumi-shibata <241448ta@gmail.com>
2020-08-09 19:23:43 +0900

    1周目学習終了

--- script.js
+++ script.js
@@ -13,4 +13,25 @@ $(function() {
       },300);
     }
   );
+
+
+  // scrollTopメソッド: $('html, body').scrollTop(値);  ページ最上部から値pxの位置に移動することができる。最上部は「０」
+  // $('html, body').scrollTop(0);
+
+  // アニメーションを用いたスクロール
+  $('#top-btn').click(function() {
+  	$('html, body').animate({
+  		'scrollTop':0
+  	},500);
+  });
+
+
+  // offsetメソッド: ページの上端からの距離とページの左端からの距離が連想配列の形で返ってくる。offset().topとすると、ページの上端からの距離が取得できる。
+  $('header a').click(function() {
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html,body').animate({
+      'scrollTop':position
+    },500);
+  });
 });
