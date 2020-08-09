3b5e6867708464da6c016eaa8b468f67c7592405
takumi-shibata <241448ta@gmail.com>
2020-08-09 18:15:01 +0900

    1周目アニメーションの付与終了

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-  // animateメソッド: アニメーションの付与、$('セレクタ').animate({'プロパティ':'値'}第2引数);
+$(function() {
+  // animateメソッド: アニメーションの付与、$('セレクタ').animate({'プロパティ':'値'},第2引数);
   // ※第2引数でアニメーションの時間を設定することができる。時間はミリ秒で指定するか、'slow'や'fast'といった文字列で指定することができる。
+  $('.social-icon').hover(
+    function() {
+      $(this).animate({
+        'font-size':'30px'
+      },300);
+    },
+    function() {
+      $(this).animate({
+        'font-size':'24px'
+      },300);
+    }
+  );
+});
